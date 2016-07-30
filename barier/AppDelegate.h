cc69732dc264befdea75eb50b4f6845b9a5656d6
shihua <1506057560@qq.com>
2016-07-30 13:01:05 +0800

    Initial Commit

---
+++ barier/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  barier
+//
+//  Created by shihua on 16/7/30.
+//  Copyright © 2016年 shihua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ barier/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  barier
+//
+//  Created by shihua on 16/7/30.
+//  Copyright © 2016年 shihua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
