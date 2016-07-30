1880e818c274a58bd26cd04e0c4eafec56c71f30
shihua <1506057560@qq.com>
2016-07-30 19:51:40 +0800

    mm

--- barier/ViewController.m
+++ barier/ViewController.m
@@ -9,16 +9,31 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic, strong) NSMutableArray* arrayM;
+@property (nonatomic, strong) dispatch_queue_t queue;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    //初始化数组与队列
+    self.arrayM = [NSMutableArray array];
+    self.queue = dispatch_queue_create("que" , DISPATCH_QUEUE_CONCURRENT);
     // Do any additional setup after loading the view, typically from a nib.
 }
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [self loadImage];
+}
+- (void)loadImage {
+    //往线程里添加图片
+    for (int i = 0; i<20; i++) {
+        dispatch_async(self.queue, ^{
+            NSString *name = [NSString stringWithFormat:@"%02d.jpg",i%10+1];
+        });
+    }
 
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
